65905a639ded5e2987910a4d3d2896ac564a67cc
deliciousqoo <deliciousqoo@naver.com>
2020-06-03 13:33:09 +0900

    Version1

---
+++ Source/newProject/newProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "newProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWPROJECT_API AnewProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
